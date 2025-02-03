c5db70dbe1d5636574984ac13ddd78b7869261f6
Samuel Habib <samuel.15.fahim@gmail.com>
2025-02-02 21:35:21 -0500

    ready for deploymnet

--- app.js
+++ app.js
@@ -4,17 +4,13 @@ const db = require("./config/db.js")
 const path = require('path');
 const app = express();
 
-
-// require('./resend/welcome-email.js')
-// require('./resend/invitation-email.js)
-
 const cors = require('cors');
 app.use(cors({
-    origin: 'http://localhost:3000', // Change this to your frontend URL in production
+    origin: process.env.URL ||'http://localhost:3000', // Change this to your frontend URL in production
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Allows cookies/session headers if applicable
 }));
-app.options('*', cors()); // Handles preflight requests
+app.options(process.env.URL, cors());
 
 db()
 
@@ -24,10 +20,6 @@ app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public', 'css')));
-app.use(express.static(path.join(__dirname, 'public', 'js')));
-
-
 
 // API ROUTES
 const personRoutes = require('./routes/personRoutes').router;
@@ -44,12 +36,17 @@ app.use("/auth", loginRoutes);
 app.use((req, res, next) => {
     res.setHeader(
         "Content-Security-Policy",
-        "default-src 'self'; img-src 'self';"
+        "default-src 'self'; img-src 'self' data: https:;"
     );
     next();
 });
 
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Internal Server Error' });
+});
+
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0' ,  () => {
     console.log(`Server is running on port ${PORT}`);
 });
--- resend/invitation-template.js
+++ resend/invitation-template.js
@@ -1,4 +1,4 @@
-export const generateInviteEmail = (recipient, inviteCode) => {
+const generateInviteEmail = (recipient, inviteCode) => {
     return `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
     <html xmlns="http://www.w3.org/1999/xhtml" lang="en" dir="ltr">
     <head>
